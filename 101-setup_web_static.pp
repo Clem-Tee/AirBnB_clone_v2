05519ee941fc31cda58160910d1c53050bf83915
Clem-Tee <clementogol@gmail.com>
2022-03-25 00:18:37 +0300

    Updated advanced task

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -8,7 +8,7 @@ exec { '/usr/bin/env apt-get -y update' : }
 -> exec { '/usr/bin/env service nginx start' : }
 -> exec { '/usr/bin/env mkdir -p /data/web_static/releases/test/' : }
 -> exec { '/usr/bin/env mkdir -p /data/web_static/shared/' : }
--> exec { '/usr/bin/env echo "Hello Holberton School!" > /data/web_static/releases/test/index.html' : }
+-> exec { '/usr/bin/env echo "Holberton School!" > /data/web_static/releases/test/index.html' : }
 -> exec { '/usr/bin/env ln -sf /data/web_static/releases/test/ /data/web_static/current' : }
 -> exec { '/usr/bin/env sed -i "/^\tlocation \/ {$/ i\\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n}" /etc/nginx/sites-available/default' : }
 -> exec { '/usr/bin/env service nginx restart' : }
