f8cf46289fb30524f21c6b9b1bdda54b977cd776
Clem-Tee <clementogol@gmail.com>
2022-03-24 23:52:32 +0300

    Updated file

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -9,7 +9,7 @@ sed -i "/redirect_me/ a\\\terror_page 404 /custom_404.html;" /etc/nginx/sites-av
 service nginx start
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
-echo "Hello Holberton School!" > /data/web_static/releases/test/index.html
+echo "Holberton School!" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
 sed -i "/^\tlocation \/ {$/ i\\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n}" /etc/nginx/sites-available/default
